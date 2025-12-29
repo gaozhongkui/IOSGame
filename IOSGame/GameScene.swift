600d55e7cd1d5bf4f5decc3397dc0bd61389dd18
gaozhongkui <gaozhongkui@meetstarinc.com>
2025-12-29 16:16:56 +0800

    新增：量杯的图片展示

--- IOSGame/GameScene.swift
+++ IOSGame/GameScene.swift
@@ -9,21 +9,19 @@ import GameplayKit
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMove(to view: SKView) {}
-    
-    func touchDown(atPoint pos: CGPoint) {}
-    
-    func touchMoved(toPoint pos: CGPoint) {}
-    
-    func touchUp(atPoint pos: CGPoint) {}
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {}
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {}
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {}
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {}
-    
-    override func update(_ currentTime: TimeInterval) {}
+    override func didMove(to view: SKView) {
+        backgroundColor = .white
+        let bottle = createBottle()
+        addChild(bottle)
+    }
+
+    private func createBottle() -> SKNode {
+        let bottle = SKSpriteNode(imageNamed: "bottle")
+        let bottleHeight: CGFloat = 400
+        let bottleWidth = (bottle.size.width * bottleHeight) / bottle.size.height
+        bottle.size = CGSize(width: bottleWidth, height: bottleHeight)
+        bottle.texture?.filteringMode = .linear
+        return bottle
+    }
 }
+
